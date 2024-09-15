ae6b36ea25bc8a8618461558f3ae4baa2fe683ac
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-15 22:16:10 +0300

    Update Dehumidifier table name and add some validations

--- src/main/java/com/aposiamp/smartlivingservice/models/Dehumidifier.java
+++ src/main/java/com/aposiamp/smartlivingservice/models/Dehumidifier.java
@@ -2,16 +2,28 @@ package com.aposiamp.smartlivingservice.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "dehumidifiers")
+@Table(name = "Dehumidifiers")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Dehumidifier extends Device {
+    @NotNull
+    @Min(value = 40)
+    @Max(value = 95)
     private Integer humidityLevel;
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 5)
     private Integer fanSpeed;
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 650)
     private Integer powerConsumption;
 }
