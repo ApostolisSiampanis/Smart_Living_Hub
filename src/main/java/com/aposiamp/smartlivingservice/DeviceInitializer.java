0ae802620335bc18a2b58c85be93ab86faffbf4c
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-15 22:40:40 +0300

    Create some new devices

--- src/main/java/com/aposiamp/smartlivingservice/DeviceInitializer.java
+++ src/main/java/com/aposiamp/smartlivingservice/DeviceInitializer.java
@@ -33,8 +33,44 @@ public class DeviceInitializer {
             ac1.setTemperature(25);
             ac1.setPowerConsumption(1000);
 
-            // Save to db
             ac1.persist();
+
+            AirCondition ac2 = new AirCondition();
+            ac2.setId("AC-6378");
+            ac2.setType(DeviceType.AIR_CONDITIONER);
+            ac2.setState(DeviceState.OFF);
+            ac2.setMode(DeviceMode.COOL);
+            ac2.setAirDirection(3);
+            ac2.setFanSpeed(4);
+            ac2.setTemperature(20);
+            ac2.setPowerConsumption(1500);
+
+            ac2.persist();
+
+
+            AirCondition ac3 = new AirCondition();
+            ac3.setId("AC-7489");
+            ac3.setType(DeviceType.AIR_CONDITIONER);
+            ac3.setState(DeviceState.ON);
+            ac3.setMode(DeviceMode.DRY);
+            ac3.setAirDirection(2);
+            ac3.setFanSpeed(3);
+            ac3.setTemperature(22);
+            ac3.setPowerConsumption(1200);
+
+            ac3.persist();
+
+            AirCondition ac4 = new AirCondition();
+            ac4.setId("AC-8590");
+            ac4.setType(DeviceType.AIR_CONDITIONER);
+            ac4.setState(DeviceState.OFF);
+            ac4.setMode(DeviceMode.AUTO);
+            ac4.setAirDirection(0);
+            ac4.setFanSpeed(1);
+            ac4.setTemperature(18);
+            ac4.setPowerConsumption(2000);
+
+            ac4.persist();
         }
 
         if (Thermostat.count() == 0) {
@@ -46,6 +82,24 @@ public class DeviceInitializer {
             t1.setTemperature(20);
 
             t1.persist();
+
+            Thermostat t2 = new Thermostat();
+            t2.setId("TH-2345");
+            t2.setType(DeviceType.THERMOSTAT);
+            t2.setState(DeviceState.ON);
+            t2.setMode(DeviceMode.HEAT);
+            t2.setTemperature(25);
+
+            t2.persist();
+
+            Thermostat t3 = new Thermostat();
+            t3.setId("TH-3456");
+            t3.setType(DeviceType.THERMOSTAT);
+            t3.setState(DeviceState.OFF);
+            t3.setMode(DeviceMode.AUTO);
+            t3.setTemperature(22);
+
+            t3.persist();
         }
 
         if (Dehumidifier.count() == 0) {
@@ -59,6 +113,39 @@ public class DeviceInitializer {
             d1.setPowerConsumption(300);
 
             d1.persist();
+
+            Dehumidifier d2 = new Dehumidifier();
+            d2.setId("DEH-9832");
+            d2.setType(DeviceType.DEHUMIDIFIER);
+            d2.setState(DeviceState.ON);
+            d2.setMode(DeviceMode.AUTO);
+            d2.setHumidityLevel(60);
+            d2.setFanSpeed(4);
+            d2.setPowerConsumption(350);
+
+            d2.persist();
+
+            Dehumidifier d3 = new Dehumidifier();
+            d3.setId("DEH-0943");
+            d3.setType(DeviceType.DEHUMIDIFIER);
+            d3.setState(DeviceState.OFF);
+            d3.setMode(DeviceMode.DRY);
+            d3.setHumidityLevel(55);
+            d3.setFanSpeed(2);
+            d3.setPowerConsumption(320);
+
+            d3.persist();
+
+            Dehumidifier d4 = new Dehumidifier();
+            d4.setId("DEH-1054");
+            d4.setType(DeviceType.DEHUMIDIFIER);
+            d4.setState(DeviceState.ON);
+            d4.setMode(DeviceMode.HUMIDITY);
+            d4.setHumidityLevel(70);
+            d4.setFanSpeed(0);
+            d4.setPowerConsumption(400);
+
+            d4.persist();
         }
     }
 
