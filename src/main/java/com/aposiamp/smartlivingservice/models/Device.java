9eb65749a3c77f037b5549ef3aae45ff1b89686a
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-15 22:16:33 +0300

    Update Device table name and add some validations

--- src/main/java/com/aposiamp/smartlivingservice/models/Device.java
+++ src/main/java/com/aposiamp/smartlivingservice/models/Device.java
@@ -5,21 +5,25 @@ import com.aposiamp.smartlivingservice.enums.DeviceState;
 import com.aposiamp.smartlivingservice.enums.DeviceType;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "devices")
+@Table(name = "Devices")
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Device extends PanacheEntityBase {
 
     @Id
     private String id;
+    @NotNull
     @Enumerated(EnumType.STRING)
     private DeviceType type;
+    @NotNull
     @Enumerated(EnumType.STRING)
     private DeviceState state;
+    @NotNull
     @Enumerated(EnumType.STRING)
     private DeviceMode mode;
 }
