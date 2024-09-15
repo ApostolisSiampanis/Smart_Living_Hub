7f4385092ed9f14a28438bafc824febe8d9859bf
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-15 22:16:21 +0300

    Update Thermostat table name and add some validations

--- src/main/java/com/aposiamp/smartlivingservice/models/Thermostat.java
+++ src/main/java/com/aposiamp/smartlivingservice/models/Thermostat.java
@@ -2,14 +2,20 @@ package com.aposiamp.smartlivingservice.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "thermostats")
+@Table(name = "Thermostats")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Thermostat extends Device {
+    @NotNull
+    @Min(value = 10)
+    @Max(value = 30)
     private Integer temperature;
 }
