486983442517354d855c89a0db616059899e76cb
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-25 15:25:24 +0300

    Update DeviceResource to use DeviceService

--- src/main/java/com/aposiamp/smartlivingservice/resources/DeviceResource.java
+++ src/main/java/com/aposiamp/smartlivingservice/resources/DeviceResource.java
@@ -2,16 +2,20 @@ package com.aposiamp.smartlivingservice.resources;
 
 import com.aposiamp.smartlivingservice.enums.DeviceType;
 import com.aposiamp.smartlivingservice.models.Device;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import com.aposiamp.smartlivingservice.services.DeviceService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Optional;
 
 @Path("/devices")
 @Produces(MediaType.APPLICATION_JSON)
 public class DeviceResource {
+
+    @Inject
+    DeviceService deviceService;
 
     @GET
     @Path("/{id}")
@@ -29,15 +33,45 @@ public class DeviceResource {
     @GET
     @Path("/id-and-type/{id}/{type}")
     public Response getDeviceByIdAndType(@PathParam("id") String id, @PathParam("type") DeviceType type) {
-        Device device = Device.<Device>listAll().stream()
-                .filter(d -> d.getId().equals(id) && d.getType() == type)
-                .findFirst()
-                .orElse(null);
 
-        if (device == null) {
+        Optional<Device> deviceOptional = deviceService.getDeviceByIdAndType(id, type);
+
+        if (deviceOptional.isEmpty()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return Response.ok(device).build();
+        return Response.ok(deviceOptional.get()).build();
+    }
+
+    @PATCH
+    @Path("/{id}/state")
+    public Response updateState(@PathParam("id") String id, String newStateString) {
+
+        try {
+            deviceService.updateState(id, newStateString);
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+
+        return Response.noContent().build();
+    }
+
+    @PATCH
+    @Path("/{id}/mode")
+    public Response updateMode(@PathParam("id") String id, String newModeString) {
+
+        try {
+            deviceService.updateMode(id, newModeString);
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+
+        return Response.noContent().build();
     }
 }
