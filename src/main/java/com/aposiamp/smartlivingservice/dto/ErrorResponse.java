3a34193be1406fd1b0dfdc7112a3405164e446ad
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-25 17:14:18 +0300

    Create ErrorResponse

---
+++ src/main/java/com/aposiamp/smartlivingservice/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.aposiamp.smartlivingservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String error;
+    private String message;
+}
