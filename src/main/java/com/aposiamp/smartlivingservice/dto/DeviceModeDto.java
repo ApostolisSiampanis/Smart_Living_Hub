8ed990dcd59dda6e3634a0db4863dbd5cd94d501
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-25 17:14:31 +0300

    Create DeviceModeDto

---
+++ src/main/java/com/aposiamp/smartlivingservice/dto/DeviceModeDto.java
@@ -0,0 +1,11 @@
+package com.aposiamp.smartlivingservice.dto;
+
+import com.aposiamp.smartlivingservice.enums.DeviceMode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeviceModeDto {
+    private String deviceMode;
+}
