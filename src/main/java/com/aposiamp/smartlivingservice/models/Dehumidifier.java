316184572fc7edc2c3549f295c727e2fd613d9c3
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 21:49:56 +0300

    Update Dehumidifier

--- src/main/java/com/aposiamp/smartlivingservice/models/Dehumidifier.java
+++ src/main/java/com/aposiamp/smartlivingservice/models/Dehumidifier.java
@@ -1,5 +1,6 @@
 package com.aposiamp.smartlivingservice.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
@@ -10,16 +11,18 @@ import lombok.*;
 @Getter
 @Setter
 @Entity
-@Table(name = "Dehumidifiers")
+@Table(name = "dehumidifiers")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Dehumidifier extends Device {
     @NotNull
     @Min(value = 40)
     @Max(value = 95)
+    @Column(name = "humidity_level")
     private Integer humidityLevel;
     @NotNull
     @Min(value = 0)
     @Max(value = 5)
+    @Column(name = "fan_speed")
     private Integer fanSpeed;
 }
