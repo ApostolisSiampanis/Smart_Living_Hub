e41c4cbd2afea34c24fb9f72463266f8dbc874f1
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 21:48:00 +0300

    Update Device

--- src/main/java/com/aposiamp/smartlivingservice/models/Device.java
+++ src/main/java/com/aposiamp/smartlivingservice/models/Device.java
@@ -6,6 +6,8 @@ import com.aposiamp.smartlivingservice.enums.DeviceType;
 import com.aposiamp.smartlivingservice.validation.ValidDeviceMode;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -28,4 +30,12 @@ public abstract class Device extends PanacheEntityBase {
     @NotNull
     @Enumerated(EnumType.STRING)
     private DeviceMode mode;
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 3500)
+    @Column(name = "power_consumption")
+    private Integer powerConsumption;
+
+    @Column(name = "turned_on_at")
+    private Instant turnedOnAt;
 }
