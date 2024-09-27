0a64fb33f841550fd7292eed78f2a8e8de6c914f
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 21:49:49 +0300

    Update AirCondition

--- src/main/java/com/aposiamp/smartlivingservice/models/AirCondition.java
+++ src/main/java/com/aposiamp/smartlivingservice/models/AirCondition.java
@@ -1,5 +1,6 @@
 package com.aposiamp.smartlivingservice.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
@@ -10,17 +11,19 @@ import lombok.*;
 @Getter
 @Setter
 @Entity
-@Table(name = "AirConditions")
+@Table(name = "air_conditions")
 @AllArgsConstructor
 @NoArgsConstructor
 public class AirCondition extends Device {
     @NotNull
     @Min(value = 0)
     @Max(value = 4)
+    @Column(name = "air_direction")
     private Integer airDirection;
     @NotNull
     @Min(value = 0)
     @Max(value = 5)
+    @Column(name = "fan_speed")
     private Integer fanSpeed;
     @NotNull
     @Min(value = 16)
